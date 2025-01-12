6fa17361d97a90b46ca20efa364ddfb264218ded
Priyanka26211 <maitypriyanka514@gmail.com>
2025-01-12 17:41:42 +0530

    This is my first Commit.

---
+++ app/src/main/java/com/example/radiobutton/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.radiobutton;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.WindowDecorActionBar;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button submit,clear;
+    RadioGroup radioGroup;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        submit = findViewById(R.id.submit);
+        clear = findViewById(R.id.clear);
+        radioGroup = findViewById(R.id.radiogroup);
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton radioButton = group.findViewById(checkedId);
+            }
+        });
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int selectedId = radioGroup.getCheckedRadioButtonId();
+                RadioButton radioButton=(RadioButton)radioGroup.findViewById(selectedId);
+                Toast.makeText(MainActivity.this,radioButton.getText(),Toast.LENGTH_LONG).show();
+            }
+        });
+
+        clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                radioGroup.clearCheck();
+            }
+        });
+    }
+}
